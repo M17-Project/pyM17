726ff7be773a3856598ea0f71c5e333db48a5a2c
Luke Stamm <we3dz@we3dz.com>
2025-01-16 11:22:24 -0800

    cleaned address

--- src/m17/m17/address.py
+++ src/m17/m17/address.py
@@ -1,32 +1,13 @@
-import sys
 import string
-import unittest
+import sys
 
 import bitstruct
 
-from .misc import _x
-callsign_alphabet = " " + string.ascii_uppercase + string.digits + "-/." 
-#"." is TBD
+CALLSIGN_ALPHABET = " " + string.ascii_uppercase + string.digits + "-/."
+# "." is TBD
 # print("Alphabet: %s"%(callsign_alphabet))
 # print("len(Alphabet): %d"%(len(callsign_alphabet)))
 
-
-class test_address(unittest.TestCase):
-    def setUp(self):
-        self.me = Address(callsign="W2FBI")
-        self.ref = Address(callsign="XLX307 D")
-    def test_string_compare(self):
-        self.assertEqual(self.me, "W2FBI")
-    def test_num_compare(self):
-        self.assertEqual(self.me, 23178783)
-    def test_compare(self):
-        me2 = Address(addr=23178783)
-        self.assertEqual(self.me, me2)
-    def test_not_equal(self):
-        me2 = Address(addr=23178784)
-        self.assertNotEqual(self.me,me2)
-    def test_bytes(self):
-        self.assertEqual(bytes(self.me), b'\x00\x00\x01a\xae\x1f')
 
 class Address:
     """
@@ -64,26 +45,28 @@ class Address:
 
 
     """
+
     def __init__(self, **kwargs):
-        for k,v in kwargs.items():
-            if k in ["addr","callsign"]:
-                setattr(self,k,v)
-        self.callsign = self.callsign.upper() if hasattr(self,"callsign") else self.decode(self.addr)
-        self.addr = self.addr if hasattr(self,"addr") else self.encode(self.callsign)
+        for k, v in kwargs.items():
+            if k in ["addr", "callsign"]:
+                setattr(self, k, v)
+        self.callsign = self.callsign.upper() if hasattr(self, "callsign") else self.decode(self.addr)
+        self.addr = self.addr if hasattr(self, "addr") else self.encode(self.callsign)
 
     def __str__(self):
-        return "%s == 0x%06x"%(self.callsign,self.addr)
+        return "%s == 0x%06x" % (self.callsign, self.addr)
+
     def __bytes__(self):
-        return bitstruct.pack("u48",self.addr)
+        return bitstruct.pack("u48", self.addr)
 
     def __index__(self):
         return self.addr
 
     def __eq__(self, compareto):
         if type(compareto) == type(""):
-            if compareto.isdigit(): #yeah, gross.
+            if compareto.isdigit():  # yeah, gross.
                 return int(compareto) == self.addr
-            else: 
+            else:
                 return compareto.upper() == self.callsign
         elif type(compareto) == type(1):
             return compareto == self.addr
@@ -94,48 +77,77 @@ class Address:
 
     @staticmethod
     def to_dmr_id(something):
+        """
+        Convert a callsign to a DMR ID
+        """
         # if no db:
         # url = "https://database.radioid.net/static/users.json"
         # requests.get()
-        #if db but not found, _check once_ using https://database.radioid.net/api/dmr/user/?id=3125404
-        #return an Address encoded for DMR using database lookup?
-        #or jsut the ID as an int?
+        # if db but not found, _check once_ using https://database.radioid.net/api/dmr/user/?id=3125404
+        # return an Address encoded for DMR using database lookup?
+        # or jsut the ID as an int?
         ...
+
     @staticmethod
     def from_dmr_id(dmr_int):
-        #return an Address encoded for callsign using dmr database lookup to get callsign
+        """
+        Convert a DMR ID to a callsign
+        """
+        # return an Address encoded for callsign using dmr database lookup to get callsign
         ...
+
     def is_dmr_id(self):
+        """
+        Is this a DMR ID?
+        """
         return self.callsign.startswith("D") and self.callsign[1:].isdigit()
+
     def is_dmr_talkgroup(self):
+        """
+        Is this a DMR talkgroup?
+        """
         return any(
-                self.is_brandmeister_tg()
-                )
+            self.is_brandmeister_tg()
+        )
+
     def is_brandmeister_tg(self):
+        """
+        Is this a Brandmeister talkgroup?
+        """
         return self.callsign.startswith("BM") and self.callsign[1:].isdigit()
+
     def is_dstar_reflector(self):
+        """
+        Is this a D-Star reflector?
+        """
         return self.callsign.startswith("REF")
 
     @staticmethod
     def encode(callsign):
+        """
+        Encode a callsign into an address
+        """
         num = 0
         for char in callsign.upper()[::-1]:
-            charidx = callsign_alphabet.index(char)
+            charidx = CALLSIGN_ALPHABET.index(char)
             num *= 40
             num += charidx
-            if num >= 40**9:
-                raise(Exception("Invalid callsign"))
+            if num >= 40 ** 9:
+                raise ValueError("Invalid callsign")
         return num
-    
+
     @staticmethod
     def decode(addr):
+        """
+        Decode an address into a callsign
+        """
         num = addr
-        if num >= 40**9:
-            raise(Exception("Not a callsign"))
+        if num >= 40 ** 9:
+            raise ValueError("Invalid address")
         chars = []
         while num > 0:
-            idx = int(num%40)
-            c =  callsign_alphabet[idx] 
+            idx = int(num % 40)
+            c = CALLSIGN_ALPHABET[idx]
             chars.append(c)
             # print(num,idx,c)
             num //= 40
@@ -144,9 +156,14 @@ class Address:
 
 
 def show_help():
+    """
+    Show help
+    """
     print("""
 Provide callsigns on the command line and they will be translated into M17 addresses
     """)
+
+
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         show_help()
---
+++ src/m17/tests/test_address.py
@@ -0,0 +1,44 @@
+import unittest
+
+from m17.address import Address
+
+
+class TestAddress(unittest.TestCase):
+    """
+    Tests for the Address class
+    """
+    def setUp(self):
+        self.me = Address(callsign="W2FBI")
+        self.ref = Address(callsign="XLX307 D")
+
+    def test_string_compare(self):
+        """
+        Test that a string compares to the callsign
+        """
+        self.assertEqual(self.me, "W2FBI")
+
+    def test_num_compare(self):
+        """
+        Test that a number compares to the address
+        """
+        self.assertEqual(self.me, 23178783)
+
+    def test_compare(self):
+        """
+        Test that an Address object compares to another Address object
+        """
+        me2 = Address(addr=23178783)
+        self.assertEqual(self.me, me2)
+
+    def test_not_equal(self):
+        """
+        Test that an Address object compares to another Address object
+        """
+        me2 = Address(addr=23178784)
+        self.assertNotEqual(self.me, me2)
+
+    def test_bytes(self):
+        """
+        Test that an Address object can be compared to bytes
+        """
+        self.assertEqual(bytes(self.me), b'\x00\x00\x01a\xae\x1f')
