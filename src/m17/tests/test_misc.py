7a4481ca64d22f7408180950b4a99898bf7d1a97
Luke Stamm <we3dz@we3dz.com>
2025-01-28 18:45:49 -0800

    all tests passing

--- src/m17/tests/test_misc.py
+++ src/m17/tests/test_misc.py
@@ -1,12 +1,13 @@
 from m17.misc import print_4bits, print_bits, print_8bits, print_16bits
 
 
-def test_b(x):
+def test_b():
     """
     Test the binary print functions
     :param x: number to test
 
     """
+    x = 0b1010
     print(print_4bits(int(x)))
     print(print_bits(int(x)))
     print(print_8bits(int(x)))
